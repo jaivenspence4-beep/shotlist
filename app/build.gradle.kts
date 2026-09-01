97c991c80f51e7092fc7d0d097981655dd97a4b0
jaivenspence4-beep <jaivenspence4@gmail.com>
2026-08-31 22:43:22 -0700

    Target Android 16 API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "app.shotlist"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "app.shotlist"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
         vectorDrawables.useSupportLibrary = true
